63e6ea9b762e5615a96bb8d2dddf74d110e65be1
PAWIU <70094237+PAW122@users.noreply.github.com>
2023-03-21 11:19:34 +0100

    to do update

--- web/app.js
+++ web/app.js
@@ -5,14 +5,24 @@ function main(port, client) {
   const app = express();
 
   app.use(express.static(path.join(__dirname, 'src/main')));
+  app.use(express.static(path.join(__dirname, 'src/help')));
+  app.use(express.static(path.join(__dirname, 'src/dashboard')));
 
   app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './src/main/index.html'));
   });
 
+  app.get('/help', function (req, res) {
+    res.sendFile(path.join(__dirname, './src/dashboard/index.html'));
+  });
+
+  app.get('/dashboard', function (req, res) {
+    res.sendFile(path.join(__dirname, './src/dashboard/index.html'));
+  });
+
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-    console.log(`http://localhost:${port}`)
+    console.log(`http://localhost:${port} /n http://localhost:${port}/help`)
   });
 }
 
